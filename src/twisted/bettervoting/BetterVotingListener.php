25f36a8b1651b0e56f80606d43307c107db47bc4
alvin0319 <alvin7810@naver.com>
2022-03-12 16:23:02 +0900

    Update: BetterVoting

---
+++ src/twisted/bettervoting/BetterVotingListener.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types=1);
+
+namespace twisted\bettervoting;
+
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\scheduler\ClosureTask;
+use twisted\bettervoting\thread\BetterVotingThread;
+
+class BetterVotingListener implements Listener{
+
+
+	public function __construct(private BetterVoting $plugin){
+
+	}
+
+	public function onPlayerJoin(PlayerJoinEvent $event) : void{
+		$player = $event->getPlayer();
+		if(BetterVotingCache::hasUnclaimedVote($player)){
+			$thread = $this->plugin->getVoteThread();
+			$this->plugin->getScheduler()->scheduleDelayedTask(new ClosureTask(static function() use ($player, $thread) : void{
+				if($player->isOnline()){
+					$thread->addActionToQueue(BetterVotingThread::ACTION_CLAIM_VOTE, $player);
+				}
+			}), 20);
+		}
+	}
+}
---
+++ src/twisted/bettervoting/console/VoteConsole.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace twisted\bettervoting\console;
+
+use pocketmine\command\CommandSender;
+use pocketmine\lang\Language;
+use pocketmine\lang\Translatable;
+use pocketmine\permission\DefaultPermissions;
+use pocketmine\permission\PermissibleBase;
+use pocketmine\permission\PermissibleDelegateTrait;
+use pocketmine\Server;
+
+final class VoteConsole implements CommandSender{
+	use PermissibleDelegateTrait;
+
+	public function __construct(){
+		$this->perm = new PermissibleBase([DefaultPermissions::ROOT_OPERATOR => true]);
+		$this->recalculatePermissions();
+	}
+
+	public function getLanguage() : Language{
+		return $this->getServer()->getLanguage();
+	}
+
+	public function sendMessage(Translatable|string $message) : void{
+		$this->getServer()->getLogger()->info($message);
+	}
+
+	public function getServer() : Server{
+		return Server::getInstance();
+	}
+
+	public function getName() : string{
+		return "CONSOLE";
+	}
+
+	public function getScreenLineHeight() : int{
+		return PHP_INT_MAX;
+	}
+
+	public function setScreenLineHeight(?int $height) : void{
+	}
+}
